6761a7630aa16c6d250ea669678b80036c752962
Baal89 <efestos89@gmail.com>
2020-07-22 17:57:03 +0000

    delete book functinality added

--- app.py
+++ app.py
@@ -62,6 +62,12 @@ def update_book(book_id):
     return redirect(url_for('index'))
 
 
+@app.route('/delete_book/<book_id>')
+def delete_book(book_id):
+    mongo.db.books.remove({'_id': ObjectId(book_id)})
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
